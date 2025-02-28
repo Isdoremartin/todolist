0c41c1b054dbce708917148abe5a2bde0dcf59e9
Isdoremarin <martinnwaka6@gmail.com>
2025-02-28 16:01:17 +0100

    second commit

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -4,8 +4,9 @@ const TodoInput = (props) => {
   const {handleTodo, value, setValue, edit, handleUpdate} = props
   
   return (
-    <div className='main-input'>
-      <div className='input-group'>
+    <div className='main-input mb-4'>
+      <span className='my-4'>A smart and intuitive Todo app designed to enhance your lottery experience. Our app helps you stay ahead in the game.</span>
+      <div className='input-group mt-5'>
         <input className='form-control py-2'  value={value} onChange={e =>{setValue(e.target.value)}} placeholder='Enter your task' />
         
         <button className='btn btn-primary' onClick={() =>{
